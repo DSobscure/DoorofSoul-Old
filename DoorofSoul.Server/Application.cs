80af556ce9ed41ff50e0a2358d51d9f08636c54b
DSobscure <snbruce13@yahoo.com.tw>
2016-07-12 12:46:02 +0800

    Infrastructure
    build infrastructure of the project

---
+++ DoorofSoul.Server/Application.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using log4net.Config;
+using ExitGames.Logging.Log4Net;
+using ExitGames.Logging;
+using System.IO;
+
+namespace DoorofSoul.Server
+{
+    public class Application : ApplicationBase
+    {
+        private static Application instance;
+        public static Application Instance { get { return instance; } }
+
+        public static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        protected override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
